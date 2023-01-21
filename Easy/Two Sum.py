f7de924f977e94d0fa945b7ff0aeabb5ae3f86a5
Ravshan <ravshanallamurotov1@gmail.com>
2023-01-21 15:44:04 +0500

    LeetCode

---
+++ Easy/Two Sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i in nums:
+            indexx=nums.index(i)
+            if target - i in nums and nums.index(target-i)!=indexx:
+                a=target-i
+                a=nums.index(a)
+                i=nums.index(i)
+                lst=[a, i]
+                return lst
+                
